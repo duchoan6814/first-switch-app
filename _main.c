64ae0c824027d08d756205ee2eecd883b75deb71
Hoan Truong <hoantd@olabs.net>
2026-01-02 01:36:32 +0000

    init and create sum application

---
+++ _main.c
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <switch.h>
+
+const char* const months[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+const char* const weekDays[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+int main(int argc, char **argv)
+{
+    consoleInit(NULL);
+
+    // Configure our supported input layout: a single player with standard controller styles
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    // Initialize the default gamepad (which reads handheld mode inputs as well as the first connected controller)
+    PadState pad;
+    padInitializeDefault(&pad);
+
+    printf("\x1b[16;16HPress PLUS to exit.");
+    printf("\x1b[17;16HTZ=%s\n", getenv("TZ"));
+
+    // Main loop
+    while(appletMainLoop())
+    {
+        // Scan the gamepad. This should be done once for each frame
+        padUpdate(&pad);
+
+        // padGetButtonsDown returns the set of buttons that have been newly pressed in this frame compared to the previous one
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus) break; // break in order to return to hbmenu
+
+        //Print current time
+        time_t unixTime = time(NULL);
+        struct tm* timeStruct = localtime((const time_t *)&unixTime); // Gets local time. If you want UTC use gmtime().
+
+        int hours = timeStruct->tm_hour;
+        int minutes = timeStruct->tm_min;
+        int seconds = timeStruct->tm_sec;
+        int day = timeStruct->tm_mday;
+        int month = timeStruct->tm_mon;
+        int year = timeStruct->tm_year +1900;
+        int wday = timeStruct->tm_wday;
+
+        printf("\x1b[1;1H%02i:%02i:%02i", hours, minutes, seconds);
+        printf("\n%s %s %i %i", weekDays[wday], months[month], day, year);
+
+        consoleUpdate(NULL);
+    }
+
+    consoleExit(NULL);
+    return 0;
+}
---
+++ source/main.c
@@ -0,0 +1,146 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <switch.h>
+
+bool getInputFormKeyboard(int *out_number);
+
+bool getInputFormKeyboard(int *out_number)
+{
+    SwkbdConfig kbd;
+    Result rc;
+    char out_string[256];
+
+    consoleInit(NULL);
+
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    PadState pad;
+    padInitializeDefault(&pad);
+
+    rc = swkbdCreate(&kbd, 0);
+
+    if (R_FAILED(rc))
+    {
+        printf("Failed to create software keyboard: 0x%x\n", rc);
+    }
+
+    if (R_SUCCEEDED(rc))
+    {
+        swkbdConfigSetHeaderText(&kbd, "Enter your name:");
+        swkbdConfigSetStringLenMax(&kbd, 255);
+        swkbdConfigSetType(&kbd, SwkbdType_NumPad);
+
+        rc = swkbdShow(&kbd, out_string, sizeof(out_string));
+
+        swkbdClose(&kbd);
+
+        if (R_SUCCEEDED(rc))
+        {
+            *out_number = atoi(out_string);
+            return true;
+        }
+        if (R_FAILED(rc))
+        {
+            printf("Failed to show software keyboard: 0x%x\n", rc);
+            return false;
+        }
+    }
+
+    return false;
+}
+
+int main(int argc, char *argv[])
+{
+    int a = 0;
+    int b = 0;
+    int step = 0;
+
+    consoleInit(NULL);
+
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+
+    PadState pad;
+    padInitializeDefault(&pad);
+
+    printf(
+        CONSOLE_ESC(1; 1H)
+            CONSOLE_ESC(1m) "Xin chao ban!, day la mot ung dung don gian tren Nintendo Switch.");
+    printf(
+        CONSOLE_ESC(3; 1H) "Nhan A de mo ban phim va nhap so thu nhat: ");
+
+    while (appletMainLoop())
+    {
+        padUpdate(&pad);
+
+        u64 kDown = padGetButtonsDown(&pad);
+
+        if (kDown & HidNpadButton_Plus)
+            break;
+        if (kDown & HidNpadButton_A)
+        {
+            if (step == 0)
+            {
+                if (getInputFormKeyboard(&a))
+                {
+                    printf(
+                        CONSOLE_ESC(3; 1H)
+                            CONSOLE_ESC(2K)
+
+                                "So thu nhat la: " CONSOLE_ESC(1; 37; 41m) "%d" CONSOLE_ESC(0m),
+                        a);
+                    printf(
+                        CONSOLE_ESC(5; 1H) "Nhan A de mo ban phim va nhap so thu hai: ");
+                    step = 1;
+                }
+            }
+            else if (step == 1)
+            {
+                if (getInputFormKeyboard(&b))
+                {
+                    printf(
+                        CONSOLE_ESC(5; 1H)
+                            CONSOLE_ESC(2K)
+
+                                "So thu hai la: " CONSOLE_ESC(1; 37; 41m) "%d" CONSOLE_ESC(0m),
+                        b);
+                    printf(
+                        CONSOLE_ESC(7; 1H)
+                            CONSOLE_ESC(2K) "Nhan A de tinh tong hai so.");
+                    step = 2;
+                }
+            }
+            else if (step == 2)
+            {
+                int sum = a + b;
+                printf(
+                    CONSOLE_ESC(7; 1H)
+                        CONSOLE_ESC(2K)
+
+                            CONSOLE_ESC(1; 32; 40m) "Tong cua %d va %d la: %d" CONSOLE_ESC(0m),
+                    a, b, sum);
+                printf(
+                    CONSOLE_ESC(9; 1H) "Nhan A de nhap lai hai so.");
+
+                step = 3;
+            }
+            else if (step == 3)
+            {
+                a = 0;
+                b = 0;
+                step = 0;
+                printf(
+                    CONSOLE_ESC(3; 1H)
+                        CONSOLE_ESC(0J));
+                printf(
+                    CONSOLE_ESC(3; 1H) "Nhan A de mo ban phim va nhap so thu nhat: ");
+            }
+        }
+
+        consoleUpdate(NULL);
+    }
+
+    consoleExit(NULL);
+    return 0;
+}
