e4a16259f0f98708c41867f3d6074069487d0556
Hoan Truong <hoantd@olabs.net>
2026-01-02 02:51:12 +0000

    update style

--- source/menu_utils.c
+++ source/menu_utils.c
@@ -1,44 +1,19 @@
 #include <stdio.h>
 #include "menu_utils.h"
+#include "style.h"
 
 #include <switch.h>
-
-#define HOME "\x1b[1;1H"
-#define SELECTED "\x1b[1;32;40m"
-#define RESET "\x1b[0m"
-#define BOLD "\x1b[1m"
 
 void draw_menu(int activated_item)
 {
     printf(HOME);
     printf(BOLD "==== Main Menu ====\n" RESET);
 
-    if (activated_item == 0)
-    {
-        printf(SELECTED "1. Tinh tong hai so" RESET "\n");
-    }
-    else
-    {
-        printf("1. Tinh tong hai so\n");
-    }
+    printf("%s1. Tinh tong hai so%s\n", activated_item == 0 ? SELECTED_STYLE : UNSELECTED_STYLE, RESET);
 
-    if (activated_item == 1)
-    {
-        printf(SELECTED "2. Danh sach tai khoan" RESET "\n");
-    }
-    else
-    {
-        printf("2. Danh sach tai khoan\n");
-    }
+    printf("%s2. Danh sach tai khoan%s\n", activated_item == 1 ? SELECTED_STYLE : UNSELECTED_STYLE, RESET);
 
-    if (activated_item == 2)
-    {
-        printf(SELECTED "3. Thoat ung dung" RESET "\n");
-    }
-    else
-    {
-        printf("3. Thoat ung dung\n");
-    }
+    printf("%s3. Thoat ung dung%s\n", activated_item == 2 ? SELECTED_STYLE : UNSELECTED_STYLE, RESET);
 }
 
 void menu_handle_input(PadState *pad, int *activated_item, int *selected_item, int num_items)
